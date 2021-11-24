a545df8504fdddcc1863b49ea9e471571a70bf23
Combatd <combatdstar@gmail.com>
2021-11-23 20:12:07 -0800

    Define Recursion#calculateIterative

--- IterationVSRecursion/IterationVSRecursion/Program.cs
+++ IterationVSRecursion/IterationVSRecursion/Program.cs
@@ -2,11 +2,23 @@
 
 namespace IterationVSRecursion
 {
-    class Program
+    class Recursion
     {
+        public void calculateIterative(int n)
+        {
+            while(n > 0)
+            {
+                int k = n * n; // k will be n squared
+                Console.WriteLine(k);
+                n = n - 1;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Recursion r = new Recursion();
+            r.calculateIterative(4);
+            Console.ReadKey(); // Console will wait until we look at the output
         }
     }
 }
