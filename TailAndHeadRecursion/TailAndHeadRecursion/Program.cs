92dad4bdd60689efa114a3748672ff3be5e3ca68
Combatd <combatdstar@gmail.com>
2021-11-25 16:50:21 -0800

    Define Recursion#calculate

--- TailAndHeadRecursion/TailAndHeadRecursion/Program.cs
+++ TailAndHeadRecursion/TailAndHeadRecursion/Program.cs
@@ -2,8 +2,18 @@
 
 namespace TailAndHeadRecursion
 {
-    class Program
+    class Recursion
     {
+        public void calculate(int n)
+        {
+            if (n > 0) // base condition
+            {
+                int k = n * n; // n^2
+                Console.WriteLine(k);
+                calculate(n - 1); // recursive invocation
+            } 
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
