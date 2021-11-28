25d859a6de4fa608cf356198587846ae2aa0aadd
Combatd <combatdstar@gmail.com>
2021-11-27 18:02:10 -0800

    Define Recursion#calculateTree

--- TailAndHeadRecursion/TailAndHeadRecursion/Program.cs
+++ TailAndHeadRecursion/TailAndHeadRecursion/Program.cs
@@ -24,10 +24,21 @@ namespace TailAndHeadRecursion
             }
         }
 
+        public void calculateTree(int n)
+        {
+            if (n > 0) // base condition
+            {
+                calculate(n - 1); // recursive invocation
+                int k = n * n; // n^2
+                Console.WriteLine(k);
+                calculate(n - 1);
+            }
+        }
+
         static void Main(string[] args)
         {
             Recursion r = new Recursion();
-            r.calculateHead(4);
+            r.calculateTree(4);
             Console.ReadKey(); // Wait until we press a key once output shows
         }
     }
