2547ee107c8e3c636304ec320741baf884950a3b
Combatd <combatdstar@gmail.com>
2021-12-04 13:46:33 -0800

    Create SumOfNNumbers

---
+++ SumOfNNumbers/SumOfNNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOfNNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
