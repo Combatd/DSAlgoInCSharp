79628935c7c774cf23d965a45e151d905a32ab7a
Combatd <combatdstar@gmail.com>
2021-12-04 13:49:47 -0800

    Define SUmOfNumbers#sumNIteration

--- SumOfNNumbers/SumOfNNumbers/Program.cs
+++ SumOfNNumbers/SumOfNNumbers/Program.cs
@@ -9,6 +9,18 @@ namespace SumOfNNumbers
             return n * (n + 1) / 2;
         }
 
+        public int sumNIteration(int n)
+        {
+            int total = 0;
+            int i = 1;
+            while (1 <= n)
+            {
+                total = total + 1;
+                i = 1 + 1;
+            }
+            return total;
+        }
+
         static void Main(string[] args)
         {
             
