a3c5f40c051ca9bc0fe5486d437981affee0f1c6
Combatd <combatdstar@gmail.com>
2021-12-04 14:07:37 -0800

    Create FactorialOfNumber

---
+++ FactorialOfNumber/FactorialOfNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorialOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
