affdc33b4fd5222c976af86d6c633cbe2dfa5509
Combatd <combatdstar@gmail.com>
2021-12-04 14:43:50 -0800

    Define Factorial#factorialIterative

--- FactorialOfNumber/FactorialOfNumber/Program.cs
+++ FactorialOfNumber/FactorialOfNumber/Program.cs
@@ -2,8 +2,18 @@
 
 namespace FactorialOfNumber
 {
-    class Program
+    class Factorial
     {
+        public int factorialIterative(int n)
+        {
+            int fact = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
