ed518bec3877331bb44c316596b9bdd51506eb5a
Combatd <combatdstar@gmail.com>
2021-12-04 13:59:34 -0800

    Complete SumOfNNumbers

--- SumOfNNumbers/SumOfNNumbers/Program.cs
+++ SumOfNNumbers/SumOfNNumbers/Program.cs
@@ -13,10 +13,10 @@ namespace SumOfNNumbers
         {
             int total = 0;
             int i = 1;
-            while (1 <= n)
+            while (i <= n)
             {
-                total = total + 1;
-                i = 1 + 1;
+                total = total + i;
+                i = i + 1;
             }
             return total;
         }
@@ -32,7 +32,11 @@ namespace SumOfNNumbers
 
         static void Main(string[] args)
         {
-            
+            SumOfNumbers s = new SumOfNumbers();
+            Console.WriteLine("Sum " + s.sumN(5));
+            Console.WriteLine("Sum " + s.sumNIteration(5));
+            Console.WriteLine("Sum " + s.sumNRecursion(5));
+            Console.ReadKey();
         }
     }
 }
