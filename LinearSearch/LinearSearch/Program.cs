4ab4525167db183b00fc112f90a25d5e44b2af90
Combatd <combatdstar@gmail.com>
2021-12-07 00:58:56 -0800

    Define Search#linearSearch

--- LinearSearch/LinearSearch/Program.cs
+++ LinearSearch/LinearSearch/Program.cs
@@ -2,11 +2,25 @@
 
 namespace LinearSearch
 {
-    class Program
+    class Search
     {
+        public static int linearSearch(int[] A, int n, int key)
+        {
+            int index = 0; // iterator index
+            while (index < n) // O(n)
+            {
+                if(A[index] == key) // O(1)
+                {
+                    return index;
+                }
+                index = index + 1; // iterate index by 1, O(n)
+            }
+            return -1;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
