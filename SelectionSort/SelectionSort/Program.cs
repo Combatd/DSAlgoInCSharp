307a3ba68ea9ca4b287b81ec0352d4a8d76c831e
Combatd <combatdstar@gmail.com>
2021-12-11 20:20:27 -0800

    Define Sort#selectionSort

--- SelectionSort/SelectionSort/Program.cs
+++ SelectionSort/SelectionSort/Program.cs
@@ -4,9 +4,27 @@ namespace SelectionSort
 {
     class Sort
     {
+        public void selectionSort(int[] A, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                int position = i; // keep track of the "start" of iteration for comparisons
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (A[j] < A[position]) // find the largest element when making this comparison to the rest of array
+                    {
+                        position = j;
+                    }
+                }
+                int temp = A[i]; // save the start element in a temp variable
+                A[i] = A[position]; // swap temp to the new position to the right
+                A[position] = temp; // swap the start element of this iteration to the left
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
