8c0a60b7b356f9cc4067ea5f22a7324220eb5952
Combatd <combatdstar@gmail.com>
2021-12-14 20:40:32 -0800

    Define Sort#insertionSort

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -4,9 +4,24 @@ namespace InsertionSort
 {
     class Sort
     {
+        public void insertionSort(int[] A, int n)
+        {
+            for(int i = 1; i < n; i++) // we assume first element is sorted
+            {
+                int temp = A[i]; // store the value of current element
+                int position = i; // used to compare the element at A[position] with temp
+                while (position > 0 && A[position - 1] > temp)
+                {
+                    A[position] = A[position] - 1; // move elements greater than temp to the right
+                    position = position - 1; // decrement the position index
+                }
+                A[position] = temp; // now we know where the temp element needs to be inserted after everything is moved to the right
+            }
+        }
+
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
