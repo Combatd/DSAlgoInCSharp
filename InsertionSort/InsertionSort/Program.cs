63126ff1fdceac6db0fa750a0231d9677482fea4
Combatd <combatdstar@gmail.com>
2021-12-15 00:19:10 -0800

    Complete InsertionSort

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -12,7 +12,7 @@ namespace InsertionSort
                 int position = i; // used to compare the element at A[position] with temp
                 while (position > 0 && A[position - 1] > temp)
                 {
-                    A[position] = A[position] - 1; // move elements greater than temp to the right
+                    A[position] = A[position - 1]; // move elements greater than temp to the right
                     position = position - 1; // decrement the position index
                 }
                 A[position] = temp; // now we know where the temp element needs to be inserted after everything is moved to the right
@@ -30,7 +30,14 @@ namespace InsertionSort
 
         static void Main(string[] args)
         {
-            
+            Sort s = new Sort();
+            int[] arr = { 3, 5, 8, 9, 6, 2 };
+            Console.WriteLine("Original Array: ");
+            s.display(arr, 6);
+            s.insertionSort(arr, 6);
+            Console.WriteLine("Sorted Array: ");
+            s.display(arr, 6);
+            Console.ReadKey();
         }
     }
 }
