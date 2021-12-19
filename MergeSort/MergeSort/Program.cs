ba82dc3ee2b48116214df6568a8d8323d5c78f1e
Combatd <combatdstar@gmail.com>
2021-12-19 11:45:42 -0800

    Define Sort#mergeSort

--- MergeSort/MergeSort/Program.cs
+++ MergeSort/MergeSort/Program.cs
@@ -4,6 +4,18 @@ namespace MergeSort
 {
     class Sort
     {
+        public void mergeSort(int[] A, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2; // divide array into half
+                mergeSort(A, left, mid); // left
+                mergeSort(A, mid + 1, right); // right
+                // merge(A, left, mid, right);
+            }
+        }
+
+
         static void Main(string[] args)
         {
         }
