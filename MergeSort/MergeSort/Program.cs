6b107a6d5c36d897ca57caf8d386988599f1b943
Combatd <combatdstar@gmail.com>
2021-12-19 11:51:48 -0800

    Define Sort#merge

--- MergeSort/MergeSort/Program.cs
+++ MergeSort/MergeSort/Program.cs
@@ -11,10 +11,49 @@ namespace MergeSort
                 int mid = (left + right) / 2; // divide array into half
                 mergeSort(A, left, mid); // left
                 mergeSort(A, mid + 1, right); // right
-                // merge(A, left, mid, right);
+                merge(A, left, mid, right);
             }
         }
 
+        public void merge(int[] A, int left, int mid, int right)
+        {
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+            int[] B = new int[right + 1]; // new array to merge into
+
+            while(i <= mid && j <= right)
+            {
+                if (A[i] < A[j])
+                {
+                    B[k] = A[i];
+                    i = i + 1;
+                } else
+                {
+                    B[k] = A[j];
+                    j = j + 1;
+                }
+                k = k + 1;
+            }
+
+            while (i <= mid)
+            {
+                B[k] = A[i];
+                i = i + 1;
+                k = k + 1;
+            }
+
+            while (j <= right)
+            {
+                B[k] = A[j];
+                j = j + 1;
+                k = k + 1;
+            }
+            for (int x = left; x < right + 1; x++)
+            {
+                A[x] = B[x]; // copy elements of Array A into Array B
+            }
+        }
 
         static void Main(string[] args)
         {
