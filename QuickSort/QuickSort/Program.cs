9c842379da19611920591d97cb1c9e656a4bf31d
Combatd <combatdstar@gmail.com>
2021-12-22 04:41:38 -0800

    Psuedocode quickSort

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -2,6 +2,31 @@
 
 namespace QuickSort
 {
+    /* Algorithm quickSort(A, low, high)
+     * if low < high then
+     *  pi = paritition(A, low, high)
+     *  quickSort(A, low, pi - 1)
+     *  quickSort(A, pi + 1, high)
+     */
+
+    /* Algorithm partition(A, low, high)
+     *  pivot = A[low]
+     *  i = low, j = high
+     *  
+     *  do
+     *      i = i + 1
+     *  while (A[i] <= Pivot)
+     *  
+     *      do
+     *          j = j - 1
+     *      while (A[j] > pivot)
+     *          if i < j then
+     *              swap(A[i], A[j])
+     *  while (i < j)
+     *      swap(A[low], A[j])
+     *  return j
+     */
+
     class Sort
     {
         static void Main(string[] args)
