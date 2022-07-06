22bd62a7712a45ff0da1f9218b4392792b2d07e6
Combatd <combatdstar@gmail.com>
2022-07-06 09:22:49 -0700

    Define CircularLinkedList#addLast

--- CircularLinkedList/CircularLinkedList/Program.cs
+++ CircularLinkedList/CircularLinkedList/Program.cs
@@ -36,14 +36,50 @@ namespace CircularLinkedList
             return size == 0;
         }
 
-        public void addLast()
+        public void addLast(int e)
         {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+            {
+                newest.next = newest;
+                head = newest;
+            }
+            else
+            {
+                newest.next = tail.next;
+                tail.next = newest;
+            }
+            tail = newest;
+            size = size + 1;
+        }
 
+        public void display()
+        {
+            Node p = head;
+            int i = 0;
+            while(i < length())
+            {
+                Console.Write(p.element + "-->");
+                p = p.next;
+                i = i + 1;
+            }
+            Console.WriteLine();
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CircularLinkedList l = new CircularLinkedList();
+            l.addLast(7);
+            l.addLast(4);
+            l.addLast(12);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+            l.addLast(8);
+            l.addLast(3);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+
+            Console.ReadKey();
         }
     }
 }
