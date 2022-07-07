32fe73f5b4ccb493df48f0500affb14a14530edd
Combatd <combatdstar@gmail.com>
2022-07-07 12:22:50 -0700

    Define #addLast

---
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    public class Node
+    {
+        public int element;
+        public Node next;
+        public Node prev;
+
+        public Node(int e, Node n, Node p) // constructor
+        {
+            element = e;
+            next = n;
+            prev = p;
+        }
+    }
+
+    class DoublyLinkedList
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0; // initialize as empty
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null, null);
+            if(isEmpty())
+            {
+                head = newest;
+                tail = newest; // both are newest as it is the only node in the list
+            } else
+            {
+                tail.next = newest;
+                newest.prev = tail;
+                tail = newest;
+            }
+            size = size + 1;
+        }
+
+        public void display()
+        {
+            Node p = head;
+            while(p != null)
+            {
+                Console.Write(p.element + "-->");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            DoublyLinkedList l = new DoublyLinkedList();
+            l.addLast(7);
+            l.addLast(4);
+            l.addLast(12);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+            l.addLast(8);
+            l.addLast(3);
+            l.display();
+            Console.WriteLine("Size: " + l.length());
+
+            Console.ReadKey();
+        }
+    }
+}
