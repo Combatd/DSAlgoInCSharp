dcb6ba4d5a393c75ffe2f386c03805a225092834
Combatd <combatdstar@gmail.com>
2022-07-07 14:52:56 -0700

    Create Node class

--- StacksLinkedList/StacksLinkedList/Program.cs
+++ StacksLinkedList/StacksLinkedList/Program.cs
@@ -2,6 +2,17 @@
 
 namespace StacksLinkedList
 {
+    public class Node
+    {
+        public int element; // the data of the node/vertex
+        public Node next; // this will allow us to traverse the collection of Nodes that are in different memory locations
+        public Node(int e, Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
