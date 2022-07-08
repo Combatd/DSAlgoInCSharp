d2b1b2436ca7b3d08b8bfdcd4b6a701fff2c8cc9
Combatd <combatdstar@gmail.com>
2022-07-08 08:24:52 -0700

    Create Node class

--- QueueLinkedList/QueueLinkedList/Program.cs
+++ QueueLinkedList/QueueLinkedList/Program.cs
@@ -2,6 +2,18 @@
 
 namespace QueueLinkedList
 {
+    public class Node
+    {
+        int element;
+        Node next;
+
+        public Node(int e, Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
