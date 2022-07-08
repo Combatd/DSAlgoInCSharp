ca0127201700c4585bc9a7d10fa805a12ef4faa5
Combatd <combatdstar@gmail.com>
2022-07-08 13:40:20 -0700

    Define #enqueue #dequeue

--- QueueLinkedList/QueueLinkedList/Program.cs
+++ QueueLinkedList/QueueLinkedList/Program.cs
@@ -4,8 +4,8 @@ namespace QueueLinkedList
 {
     public class Node
     {
-        int element;
-        Node next;
+        public int element;
+        public Node next;
 
         public Node(int e, Node n)
         {
@@ -14,8 +14,70 @@ namespace QueueLinkedList
         }
     }
 
-    class Program
+    class QueuesLinked
     {
+        Node front;
+        Node rear;
+        int size;
+
+        public QueuesLinked()
+        {
+            front = null;
+            rear = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void enqueue(int e)
+        {
+            Node newest = new Node(e, null);
+            if(isEmpty())
+            {
+                front = newest;
+            } else
+            {
+                rear.next = newest;
+                size = size + 1;
+            }
+        }
+
+        public int dequeue()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+            int e = front.element;
+            front = front.next;
+            size = size - 1;
+            if(isEmpty())
+            {
+                rear = null;
+            }
+            return e;
+        }
+
+        public void display()
+        {
+            Node p = front;
+            while(p != null)
+            {
+                Console.Write(p.element + "-->");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
