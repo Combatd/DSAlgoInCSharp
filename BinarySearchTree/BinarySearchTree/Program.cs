759907f79cf96ae2f0619a2a9432f9a889cba035
Combatd <combatdstar@gmail.com>
2022-07-09 16:40:23 -0700

    Create Node Class for BST

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -2,7 +2,21 @@
 
 namespace BinarySearchTree
 {
-    class Program
+    public class Node
+    {
+        public int element;
+        public Node left;
+        public Node right;
+
+        public Node(int e, Node l, Node r)
+        {
+            element = e;
+            left = l;
+            right = r;
+        }
+    }
+
+    class BST
     {
         static void Main(string[] args)
         {
