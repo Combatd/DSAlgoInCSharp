8b40f79857795275d92263b93edfa04ff4d1d7c7
Combatd <combatdstar@gmail.com>
2022-07-09 16:44:26 -0700

    Define BST#insert

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -18,6 +18,45 @@ namespace BinarySearchTree
 
     class BST
     {
+        Node root;
+
+        public BST()
+        {
+            root = null;
+        }
+
+        public void insert(Node tempRoot, int e)
+        {
+            Node temp = null; // reference to the parent node to which we will be inserting a new node or element
+            while (tempRoot != null)
+            {
+                temp = tempRoot;
+                if (e == tempRoot.element) // cannot have duplicate elements
+                {
+                    return;
+                } else if (e < tempRoot.element) {
+                    tempRoot = tempRoot.left;
+                } else if (e > tempRoot.element)
+                {
+                    tempRoot = tempRoot.right;
+                }
+            }
+            Node n = new Node(e, null, null);
+            if (root != null)
+            {
+                if (e < temp.element)
+                {
+                    temp.left = n;
+                } else
+                {
+                    temp.right = n;
+                }
+            } else
+            {
+                root = n;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
